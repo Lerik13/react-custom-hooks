f3ef4e1a29e1960a48017885ce0224bd06d21a58
Valeria <lerik13@gmail.com>
2021-10-14 12:28:56 -0600

    2 hook: 'useHover' - depends on hovering change color of div

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
-import useInput from './hooks/useInput';
+import Hover from './components/Hover';
 
 const App = () => {
-	const userName = useInput('')
-	const password = useInput('')
 
 	return (
 		<div>
-			<input {...userName} type="text" placeholder="User Name"/>
-			<input {...password} type="text" placeholder="Password"/>
-			<button onClick={() => console.log(userName.value, password.value)}>Show Value</button>
+			<Hover/>
 		</div>
 	);
 };
