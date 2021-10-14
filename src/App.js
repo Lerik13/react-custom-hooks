c47d311e88d13719ac396d480c6b8ced1f4e050f
Valeria <lerik13@gmail.com>
2021-10-14 12:06:41 -0600

    1 hook: 'useInput'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useInput from './hooks/useInput';
 
 const App = () => {
+	const userName = useInput('')
+	const password = useInput('')
+
 	return (
 		<div>
-		
+			<input {...userName} type="text" placeholder="User Name"/>
+			<input {...password} type="text" placeholder="Password"/>
+			<button onClick={() => console.log(userName.value, password.value)}>Show Value</button>
 		</div>
 	);
 };
