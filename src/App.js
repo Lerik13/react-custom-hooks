e4d871071e2f46e3afc9740a1c4e0f1052de9973
Valeria <lerik13@gmail.com>
2021-11-17 12:30:30 -0700

    2 hook: 'useHover', add 1 more div with hover effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import Hover from './components/Hover';
+import useHover from './hooks/useHover';
 
 const App = () => {
+	const ref = useRef()
+	const isBlackHovering = useHover(ref)
 
 	return (
 		<div>
 			<Hover/>
+			<div ref={ref} style={{width: 300, height: 300, background: isBlackHovering ? 'blue' : 'black'}}></div>
 		</div>
 	);
 };
